694e62b891003cde23437ca65da1938657465dce
lillian <lillian0x@proton.me>
2025-06-07 17:26:01 +0900

    second

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
     };
   };
 
-  outputs = inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:{
     nixosConfigurations = {
       myNixOS = inputs.nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -17,19 +17,19 @@
         ];
       };
     };
-  };
-  homeConfigurations = {
-    myHome = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true; # プロプライエタリなパッケージを許可
+    homeConfigurations = {
+      myHome = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true; # プロプライエタリなパッケージを許可
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
       };
-      extraSpecialArgs = {
-        inherit inputs;
-      };
-      modules = [
-        ./home.nix
-      ];
     };
   };
 }
