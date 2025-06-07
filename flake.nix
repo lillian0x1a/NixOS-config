1358b2b276a7f1c197b3313ed7c847f0d7ed93dd
lillian <lillian0x@proton.me>
2025-06-07 17:25:44 +0900

    first

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs: {
+    nixosConfigurations = {
+      myNixOS = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+      };
+    };
+  };
+  homeConfigurations = {
+    myHome = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true; # プロプライエタリなパッケージを許可
+      };
+      extraSpecialArgs = {
+        inherit inputs;
+      };
+      modules = [
+        ./home.nix
+      ];
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,8 @@
+{
+  home = rec { # recでAttribute Set内で他の値を参照できるようにする
+    username="lillian";
+    homeDirectory = "/home/${username}"; # 文字列に値を埋め込む
+    stateVersion = "25.05";
+  };
+  programs.home-manager.enable = true; # home-manager自身でhome-managerを有効化
+}
